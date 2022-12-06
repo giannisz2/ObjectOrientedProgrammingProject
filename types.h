550270e514be6a8cd8aa5cb016c29b65f872ed93
Yannis Zerb <119806540+giannisz2@users.noreply.github.com>
2022-12-06 21:06:02 +0200

    Update types.h

--- types.h
+++ types.h
@@ -5,11 +5,23 @@
 
 #include "classes.h"
 
+// movement
+#define key_W 87
+#define key_S 83
+#define key_D 68
+#define key_A 65
+
+// action keys
+
+#define key_E 69 // for avatar to heal
+#define key_P 80 // for pause
+#define key_M 77
+
+
 
 typedef vector<WareWolf> warewolfVector;
 typedef vector<Vampire> vampireVector;
 typedef vector<Tree> treeVector;
 typedef vector<River> riverVector;
-
 
 
