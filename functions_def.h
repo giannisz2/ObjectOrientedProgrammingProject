b76e35c289fef49523f92847ea5cd49a209d8259
Yannis Zerb <119806540+giannisz2@users.noreply.github.com>
2022-12-07 01:34:15 +0200

    Update functions_def.h

--- functions_def.h
+++ functions_def.h
@@ -4,8 +4,13 @@
 #include <vector>
 
 #include "classes.h"
+#include "types.h"
 
 char** mapCreate(int, int);
 void printMap(char**, int, int);
 bool chooseTeam();
-void placeHazards(char**, vector<Tree>&, vector<River>&, int, int);
+void placeHazards(char**, treeVector&, riverVector&, int, int);
+void placeCharacters(char**, warewolfVector&, vampireVector&, int, int);
+void pause(warewolfVector&, vampireVector&);
+void getActionAvatar(char**, Avatar, int, int);
+void moveCharacters(char**, warewolfVector&, vampireVector&, i
