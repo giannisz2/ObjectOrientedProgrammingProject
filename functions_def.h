6b3c185b20e8c75aed776ec271cdeaaff3932f00
Yannis Zerb <119806540+giannisz2@users.noreply.github.com>
2022-12-09 20:20:48 +0200

    Update functions_def.h

--- functions_def.h
+++ functions_def.h
@@ -11,7 +11,7 @@ void printMap(char**, int, int);
 bool chooseTeam();
 void placeHazards(char**, treeVector&, riverVector&, int, int);
 void placeCharacters(char**, warewolfVector&, vampireVector&, int, int);
-void pause(warewolfVector&, vampireVector&);
+void pause(Avatar, warewolfVector&, vampireVector&);
 void getActionAvatar(char**, Avatar&, warewolfVector&, vampireVector&, int, int, int);
 void placePotion(char**, int, int);
 void moveCharacters(char**, warewolfVector&, vampireVector&, i)
